64cf240558a082847f0bf988a833ebcc9cb1e21a
Bruno Camargo <brunovieiracamargo@gmail.com>
2023-05-21 19:13:22 -0300

    feat: add findGameByKey endpoint.

--- src/main/java/com/store/controller/GameController.java
+++ src/main/java/com/store/controller/GameController.java
@@ -6,9 +6,9 @@ import com.store.service.GameService;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 
@@ -22,10 +22,19 @@ public class GameController {
     GameService gameService;
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response createGame(@Valid GameRequestDTO gameRequestDTO, @Context UriInfo uriInfo) {
         GameResponseDTO game = gameService.createGame(gameRequestDTO);
         URI uri = uriInfo.getAbsolutePathBuilder().path(game.key()).build();
         return Response.created(uri).entity(game).build();
+    }
+
+    @GET
+    @Path("{key}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findGameByKey(@PathParam("key") String key){
+        return Response.ok().entity(gameService.findGameByKey(key)).build();
     }
 
 }
--- src/main/java/com/store/service/GameService.java
+++ src/main/java/com/store/service/GameService.java
@@ -3,6 +3,7 @@ package com.store.service;
 import com.store.dto.GameRequestDTO;
 import com.store.dto.GameResponseDTO;
 import com.store.entity.Game;
+import com.store.exception.GameNotFoundException;
 import com.store.repository.GameRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -14,9 +15,20 @@ public class GameService {
     GameRepository gameRepository;
 
     @Transactional
-    public GameResponseDTO createGame(GameRequestDTO gameRequestDTO){
+    public GameResponseDTO createGame(GameRequestDTO gameRequestDTO) {
         Game entity = gameRequestDTO.toEntity();
         gameRepository.persist("game", entity);
         return new GameResponseDTO(entity);
+    }
+
+    @Transactional
+    public GameResponseDTO findGameByKey(String key){
+        Game game = gameRepository.findGameBykey(key);
+
+        if (game == null) {
+            throw new GameNotFoundException("Could not find this game in the database");
+        }
+
+        return new GameResponseDTO(game);
     }
 }
