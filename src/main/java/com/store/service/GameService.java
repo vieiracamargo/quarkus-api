cad1efea7da344fb6e9a47475dcb094ebb5f1181
bruno <brunovieiracamargo@gmail.com>
2023-06-15 14:02:16 -0300

    feat: add logs to api.

--- src/main/java/com/store/service/GameService.java
+++ src/main/java/com/store/service/GameService.java
@@ -9,11 +9,14 @@ import com.store.utils.Key;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 
 @ApplicationScoped
 public class GameService {
+
+    private static final Logger LOGGER = Logger.getLogger(GameService.class);
     @Inject
     GameRepository gameRepository;
 
@@ -22,8 +25,10 @@ public class GameService {
 
     @Transactional
     public GameResponseDTO createGame(GameRequestDTO gameRequestDTO) {
+        LOGGER.info("Saving new game");
         Game entity = gameRequestDTO.toEntity();
         gameRepository.persist(key.generate("game"), entity);
+        LOGGER.info("Game has been created with key" + entity.getKey());
         return new GameResponseDTO(entity);
     }
 
@@ -32,14 +37,17 @@ public class GameService {
         Game game = gameRepository.findGameBykey(key);
 
         if (game == null) {
+            LOGGER.error("Could not find game with key" + key + " in the database");
             throw new GameNotFoundException("Could not find this game in the database");
         }
 
+        LOGGER.info("Get the game with the key" + key);
         return new GameResponseDTO(game);
     }
 
     @Transactional
     public List<GameResponseDTO> findAllGames(){
+        LOGGER.info("Get all games inside the database");
         return gameRepository.findAllGames()
                 .stream()
                 .map(GameResponseDTO::new)
@@ -47,9 +55,11 @@ public class GameService {
     }
     @Transactional
     public GameResponseDTO updateGame(String key, GameRequestDTO gameRequestDTO){
+        LOGGER.info("Updating game data");
         findGameByKey(key);
         Game entity = gameRequestDTO.toEntity();
         gameRepository.persist(key, entity);
+        LOGGER.info("Game with key" + key + "successfully updated");
         return new GameResponseDTO(entity);
     }
 
@@ -57,5 +67,6 @@ public class GameService {
     public void deleteGame(String key){
         findGameByKey(key);
         gameRepository.deleteGame(key);
+        LOGGER.info("Game with key " + key + " successfully deleted");
     }
 }
